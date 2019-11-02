b5d1d1cb36929d05b6bbec1259c0d81cdb762678
Chase Rydeen <chase@Chases-MBP.attlocal.net>
2019-11-01 22:49:59 -0500

    Changed theme to dark mode and added navbar to get to view picks

--- routes/view-picks.js
+++ routes/view-picks.js
@@ -33,31 +33,76 @@ function game_list(games) {
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  var chase_picks;
-  var drew_picks;
-  var evan_picks;
-  var hunter_picks;
-  var shreve_picks;
+  var chase_picks={};
+  var drew_picks={};
+  var evan_picks={};
+  var hunter_picks={};
+  var shreve_picks={};
   var database = firebase.database();
   var chase=database.ref('season/' + season + '/contenders/' + 'Chase');
   chase.on('value', function(snapshot) {
-    chase_picks=snapshot.val();
+    if(snapshot.val()==undefined) {
+      for (var i = 0; i < count; i++) {
+        var new_pick_object={}
+        new_pick_object[i]="Chaos";
+        chase_picks=Object.assign(chase_picks,new_pick_object);
+      }
+    }
+    else {
+      chase_picks=snapshot.val();
+    }
   });
   var drew=database.ref('season/' + season + '/contenders/' + 'Drew');
   drew.on('value', function(snapshot) {
-    drew_picks=snapshot.val();
+    if(snapshot.val()==undefined) {
+      for (var i = 0; i < count; i++) {
+        var new_pick_object={};
+        new_pick_object[i]="Chaos";
+        drew_picks=Object.assign(drew_picks,new_pick_object);
+      }
+    }
+    else {
+      drew_picks=snapshot.val();
+    }
   });
   var evan=database.ref('season/' + season + '/contenders/' + 'Evan');
   evan.on('value', function(snapshot) {
-    evan_picks=snapshot.val();
+    if(snapshot.val()==undefined) {
+      for (var i = 0; i < count; i++) {
+        var new_pick_object={}
+        new_pick_object[i]="Chaos";
+        evan_picks=Object.assign(evan_picks,new_pick_object);
+      }
+    }
+    else {
+      evan_picks=snapshot.val();
+    }
   });
   var hunter=database.ref('season/' + season + '/contenders/' + 'Hunter');
   hunter.on('value', function(snapshot) {
-    hunter_picks=snapshot.val();
-  }); 
+    if(snapshot.val()==undefined) {
+      for (var i = 0; i < count; i++) {
+        var new_pick_object={}
+        new_pick_object[i]="Chaos";
+        hunter_picks=Object.assign(hunter_picks,new_pick_object);
+      }
+    }
+    else {
+      hunter_picks=snapshot.val();
+    }
+  });
   var shreve=database.ref('season/' + season + '/contenders/' + 'Shreve');
   shreve.on('value', function(snapshot) {
-    shreve_picks=snapshot.val();
+    if(snapshot.val()==undefined) {
+      for (var i = 0; i < count; i++) {
+        var new_pick_object={}
+        new_pick_object[i]="Chaos";
+        shreve_picks=Object.assign(shreve_picks,new_pick_object);
+      }
+    }
+    else {
+      shreve_picks=snapshot.val();
+    }
   });
   res.render('view-picks', { title: 'Welcome to the Pick Viewer', schedule:schedule, week:week, chase_picks:chase_picks, drew_picks:drew_picks, evan_picks:evan_picks, hunter_picks:hunter_picks, shreve_picks:shreve_picks});
 });
