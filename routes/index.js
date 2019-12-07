ad424518c947fb582abdef65362858214e2431d6
crydeen <dcrydeen@go.olemiss.edu>
2019-12-06 21:21:42 -0600

    Update index.js
    Week 15

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ require('firebase/database');
 const request = require('request');
 var schedule=[];
 var count=0;
-var week="14";
+var week="15";
 var year="2019";
 var season=""+year+week;
 var name='';
